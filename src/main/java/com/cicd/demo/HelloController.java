5a031c7aef9c8e668200c08185caf610aaa65470
CICD Demo <cicd@demo.com>
2026-08-10 14:35:09 +0530

    Update HelloController message to 'welcome to from Spring Boot CICD App!' and sync test cases

--- src/main/java/com/cicd/demo/HelloController.java
+++ src/main/java/com/cicd/demo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "welcome to  from Spring Boot!";
+        return "welcome to  from Spring Boot CICD App!";
     }
     
 }
